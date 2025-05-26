c68b20ddb264841e4199f5a273fa297e14949cdd
vvilip <phillipsf@tutanota.com>
2025-05-26 15:47:15 +0200

    ✨ Add Headerbar component

---
+++ src/components/Headerbar.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <v-app-bar elevation="1">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-app-bar-title>presentnow</v-app-bar-title>
+    </v-app-bar>
+</template>
+
+<style scoped>
+</style>
