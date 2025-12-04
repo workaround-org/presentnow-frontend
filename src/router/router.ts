f1cf031c398118a19af6a99464c0a2b46a6cb1ea
simonkoepf <207682734+simon-k3@users.noreply.github.com>
2025-12-04 14:14:26 +0100

    Add MyWishlists Page

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,9 +4,11 @@ import Startpage from "@/components/Startpage.vue";
 import CreateWishList from "@/components/CreateWishList.vue";
 import AuthCallback from "@/components/AuthCallback.vue";
 import ViewWishList from "@/components/ViewWishList.vue";
+import MyWishlists from "@/components/MyWishlists.vue";
 
 const routes = [
     {path: '/', component: Startpage, meta: {title: 'presentnow'}},
+    {path: '/wishlists', component: MyWishlists, meta: {title: 'My Wishlists - presentnow'}},
     {path: '/create/:wishListName', component: CreateWishList, props: true, meta: {title: 'Create Wish List - presentnow'}},
     {path: '/wishlist/:id', component: ViewWishList, props: true, meta: {title: 'View Wish List - presentnow'}},
     {path: '/callback', component: AuthCallback, meta: {title: 'Authenticating - presentnow'}}
